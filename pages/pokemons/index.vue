4c47e57d15b80d532860ee2104f3924076f6462d
lucas55i <lucas_55i@outlook.com>
2022-04-24 20:09:58 -0300

    barra de progresso de acordo com status

--- pages/pokemons/index.vue
+++ pages/pokemons/index.vue
@@ -42,16 +42,16 @@
           ></v-img>
         </v-card-text>
 
-        <v-card-text>Peso: {{ pokemons.weight }} </v-card-text>
-        <v-tabs v-model="tab" centered icons-and-text>
+        <v-tabs v-model="tab" centered>
           <v-tab> Habilidades </v-tab>
           <v-tab> Status </v-tab>
         </v-tabs>
         <br />
 
         <v-tabs-items v-model="tab">
-          <v-tab-item key="abilities"
-            ><v-card-text v-for="pokemons in abilities" :key="pokemons.url">
+          <v-tab-item key="abilities">
+            <v-card-text>Peso: {{ pokemons.weight }} </v-card-text>
+            <v-card-text v-for="pokemons in abilities" :key="pokemons.url">
               Habilidades: {{ pokemons.ability.name }}
             </v-card-text>
           </v-tab-item>
@@ -59,17 +59,15 @@
           <v-tab-item key="base">
             <v-simple-table>
               <template>
-                <thead>
-                  <tr>
-                    <th class="text-left"></th>
-                    <th class="text-left"></th>
-                  </tr>
-                </thead>
                 <tbody>
                   <tr v-for="(stats, index) in stats" :key="index">
-                    <td class="loading">
-                      {{ stats.stat.name }} - {{ stats.base_stat }}
-                      <v-progress-linear color="teal" :value="stats.base_stat">
+                    <td>{{ stats.stat.name }}</td>
+                    <td width="300">
+                      <v-progress-linear
+                        height="10"
+                        color="teal"
+                        :value="stats.base_stat"
+                      >
                       </v-progress-linear>
                     </td>
                   </tr>
@@ -78,9 +76,6 @@
             </v-simple-table>
           </v-tab-item>
         </v-tabs-items>
-
-        <v-card-text> </v-card-text>
-
         <v-divider></v-divider>
       </v-card>
     </v-dialog>
@@ -89,7 +84,7 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { Pokemons, Pokemon, Abilities } from "~/core/models/pokemon";
+import { Pokemons, Pokemon } from "~/core/models/pokemon";
 
 export default Vue.extend({
   data() {
@@ -133,6 +128,12 @@ export default Vue.extend({
         "pokemons/getPokemon",
         pokemonsName
       )) as Pokemon;
+    },
+     getTypesImage(source: string): string {
+      const sourcesImages: any = {
+        grass: '/img/grass.jpg',
+      }
+      return sourcesImages[source]
     },
   },
   created() {
