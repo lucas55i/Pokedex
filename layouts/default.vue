a58dba09e8f649d0e52e3fbc5a7b8e8be3f86ead
lucas55i <lucas_55i@outlook.com>
2022-06-11 21:37:38 -0300

    Remove footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,10 +5,7 @@
         <Nuxt />
       </v-container>
     </v-main>
-
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+ 
   </v-app>
 </template>
 
